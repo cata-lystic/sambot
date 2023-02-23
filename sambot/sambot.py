bc087fc8b23f4ac1a746265f6bffa7a11adb4d04
revanced-web-builder <ntqtmslf@protonmail.com>
2023-02-22 21:26:09 -0900

    more bot arguments: limit and shuffle

--- sambot/sambot.py
+++ sambot/sambot.py
@@ -18,17 +18,17 @@ class Samaritan(commands.Cog):
         await ctx.send(f"<:samaritan:1078116149029519491>")
 
     @commands.command()
-    async def rekon(self, ctx, choice=""):
+    async def rekon(self, ctx, query="", limit=3, shuffle=1):
         """Gets a random Sam rekon.
-        Queries
-        .rekon - Get random rekoning
-        .rekon list - Show link to rekon list
-        .rekon 25 - Show rekon by ID #
-        .rekon word - Rekon search single word
-        .rekon "multiple words" - Rekon search multiple words
+
+        **.rekon** - Get random rekoning
+        **.rekon list** - Show link to rekon list
+        **.rekon 25** - Show rekon by ID #
+        **.rekon word** - Rekon search single word
+        **.rekon "multiple words"** - Rekon search multiple words
         """
         try:
-            async with aiohttp.request("GET", "https://sambot.frwd.app?q="+choice+"&platform=discord", headers={"Accept": "text/plain"}) as r:
+            async with aiohttp.request("GET", "https://sambot.frwd.app?q="+query+"&limit="+limit+"&shuffle="+shuffle+"&platform=discord", headers={"Accept": "text/plain"}) as r:
                 if r.status != 200:
                     return await ctx.send("Oops! Cannot get a Sam rekon...")
                 result = await r.text(encoding="UTF-8")
