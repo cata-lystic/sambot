fc429b022cb572b0156dfb9c958545a5f786dabe
revanced-web-builder <ntqtmslf@protonmail.com>
2023-02-22 21:17:31 -0900

    new API args
    API shows on web. can now give ?limit=3 and ?shuffle=1.

--- web/index.php
+++ web/index.php
@@ -1,7 +1,10 @@
 <?php
 // Get possible queries
 $q = $_GET['q'] ?? null; // optionally allow a specific id to be fetched
+$limit = $_GET['limit'] ?? 3; // amount of search results to return
+$shuffle = $_GET['shuffle'] ?? 1; // whether to shuffle search results
 $platform = $_GET['platform'] ?? "web"; // anything besides "web" will have /r/n instead of <br /> between rekons
+$domain = "https://sambot.frwd.app"; // i'm lazy
 
 // Get file contents of rekons.json
 $file = "rekons.json";
@@ -18,7 +21,7 @@ if ($q == "list" && $platform == "web") {
   }
   die();
 } else if ($q == "list" && $platform != "web") {
-  echo "Full list of rekons can be found at https://sambot.frwd.app?q=list";
+  echo "Full list of rekons can be found at {$domain}?q=list";
   die();
 }
 
@@ -46,16 +49,39 @@ if ($q != null && !is_numeric($q)) {
   if (count($matches) == 0) {
     echo "No rekons found related to $q";
   } else {
-    shuffle($matches);
+    if ($shuffle == 1) shuffle($matches);
     foreach($matches as $id => $val) {
+      if ($id > $limit-1) break; // stop after the $limit
       if ($id > 0) echo ($platform == "web") ? "<br />" : "\n\r";
       echo "`".$val."`";
-      if ($id > 1) break;
     }
   }
 
 }
 
+// Show the API info box to the web users
+if ($platform == "web") {
+
+  echo "
+  <div id='info' style='position: fixed; right: 5%; bottom: 5%; background: #dddddd; padding: 3px 10px;'>
+    <h1>API</h1>
+    <p>Random rekon<br />
+    <a href='{$domain}'>{$domain}</a></p>
+
+    <p>Rekon list<br />
+    <a href='{$domain}?q=list'>{$domain}?q=list</a></p>
+
+    <p>Link to rekon list<br />
+    <a href='{$domain}?q=list&platform=discord'>{$domain}?q=list&platform=discord</a></p>
+
+    <p>Rekon by ID #<br />
+    <a href='{$domain}?q=25'>{$domain}?q=25</a> (ID #25)</p>
+
+    <p>Rekon by word search<br />
+    <a href='{$domain}?q=multiple word search&limit=3'>{$domain}?q=multiple words&limit=3&shuffle=1</a></p>
+  </div>";
+
+}
 
 
 ?>
