27f00245121f8c701cf0172cf5f200cf68287d9d
revanced-web-builder <ntqtmslf@protonmail.com>
2023-02-22 17:04:41 -0900

    ability to add a query after .rekon

--- web/index.php
+++ web/index.php
@@ -15,10 +15,32 @@ if ($q == "list") {
   die();
 }
 
-$rand = ($q == null) ? rand(0, $total-1) : $q;
-if ($rand > $total) {
-  echo "I need to rekon more to get to #".$rand;
-} else {
-  echo $data['rekon'][$rand];
+// If $q is numeric or empty, fetch a random or desired ID
+if ($q == null || is_numeric($q)) {
+  $rand = ($q == null) ? rand(0, $total-1) : $q;
+  if ($rand > $total) {
+    echo "I need to rekon more to get to #".$rand;
+  } else {
+    echo $data['rekon'][$rand];
+  }
 }
+
+// If $q is a string, search each rekon to see if that word is in it
+if ($q != null && !is_numeric($q)) {
+
+  $results = 0;
+
+  foreach ($data['rekon'] as $id => $val) {
+    if (preg_match("/{$q}/i", $val)) {
+      echo $val."\n\r";
+      $results++;
+    }
+  }
+
+  if ($results == 0) { echo "No rekons found related to $q"; }
+
+}
+
+
+
 ?>
